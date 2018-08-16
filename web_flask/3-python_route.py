8ebf0114cdfb0cb2754d2c3d12164cce23b95184
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-16 05:37:10 +0000

    task3 revise default

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -16,7 +16,7 @@ def hbnb():
 def show_text(text):
     text.replace("_", " ")
     return 'C %s' % text
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
 def show_default_text(text="is_cool"):
     text.replace("_", " ")
     return 'Python %s' % text
