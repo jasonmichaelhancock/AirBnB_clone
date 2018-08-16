d0389bc005f312944534264eeec4e13a4f1dfe01
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-16 05:16:59 +0000

    task3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''Script that starts a Flask web application.'''
+
+from flask import Flask
+
+app = Flask('__name__')
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB!'
+@app.route('/c/<text>', strict_slashes=False)
+def show_text(text):
+    text.replace("_", " ")
+    return 'C %s' % text
+@app.route('/python/<text>', strict_slashes=False)
+def show_default_text(text=cool):
+    text.replace("_", " ")
+    return 'C %s' % text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
