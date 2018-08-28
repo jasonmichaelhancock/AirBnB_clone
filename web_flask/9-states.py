1a4d6987c874a458f318664145638252eb4eafcb
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-28 22:23:40 +0000

    task10

---
+++ web_flask/9-states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''Script starts Flask web application.'''
+from models import storage
+from models import *
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    '''After each request you must remove the current SQLAlchemy Session.'''
+    storage.close()
+
+
+@app.route('/states')
+@app.route('/states_list')
+def html_fetch_states():
+    '''Display a list of states.'''
+    state_objs = [s for s in storage.all("State").values()]
+    return render_template('7-states_list.html',
+                           state_objs=state_objs)
+
+
+@app.route('/cities_by_states')
+def html_fetch_cities_by_states():
+    '''To load all cities of a State.'''
+    state_objs = [s for s in storage.all("State").values()]
+    return render_template('8-cities_by_states.html',
+                           state_objs=state_objs)
+
+
+@app.route('/states/<id>')
+def html_if_stateID(id):
+    '''Display states or a state.'''
+    state_obj = None
+    for state in storage.all("State").values():
+        if state.id == id:
+            state_obj = state
+    return render_template('9-states.html',
+                           state_obj=state_obj)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
