ff30a753276ca8a132957387fa6acfa73bb7b1a7
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-28 21:02:32 +0000

    task8 revised

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-
 '''Script that starts a Flask web application.'''
-
-from flask import Flask, render_template
-from models import *
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
 from models import storage
 from models import *
 from flask import Flask, render_template
