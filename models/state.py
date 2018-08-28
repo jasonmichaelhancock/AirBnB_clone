9f21900d3ea5fd326a16377138205ad6e7f46383
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-28 21:38:03 +0000

    task7 revise state.py

--- models/state.py
+++ models/state.py
@@ -2,21 +2,31 @@
 '''
     Implementation of the State class
 '''
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String,
+from os import getenv
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+import models
+
 
 class State(BaseModel, Base):
     '''
         Implementation for the State.
     '''
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='State', cascade='all, delete-orphan')
 
-    @property
-    def cities(self):
-        '''Getter for cities'''
-        get_all = models.storage.all('City')
-        return [obj for obj in get_all if obj.state_id == self.id]
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', back_populates='State',
+                              cascade='all, delete-orphan')
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            '''Getter for cities'''
+            list_cities = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
