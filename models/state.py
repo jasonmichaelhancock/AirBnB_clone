edd9f258d7fc7b28ecc8c7228fe4ea1ac92ccc0b
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-28 21:40:55 +0000

    task7 revise state.py

--- models/state.py
+++ models/state.py
@@ -2,8 +2,8 @@
 '''
     Implementation of the State class
 '''
-from sqlalchemy import Column, String,
 from os import getenv
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 import models
@@ -12,21 +12,27 @@ import models
 class State(BaseModel, Base):
     '''
         Implementation for the State.
+        Create relationship between class State (parent) to City (child)
     '''
     __tablename__ = "states"
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship('City', back_populates='State',
-                              cascade='all, delete-orphan')
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         name = ""
 
         @property
         def cities(self):
-            '''Getter for cities'''
+            '''
+                Return list of city instances if City.state_id==current
+                State.id
+                FileStorage relationship between State and City
+            '''
             list_cities = []
             for city in models.storage.all("City").values():
                 if city.state_id == self.id:
                     list_cities.append(city)
             return list_cities
+
